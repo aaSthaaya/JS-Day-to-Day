685b61080fd661847b325c38a54a360670303ad2
Aastha <aasthamishra175@gmail.com>
2024-02-29 15:48:39 +0545

    Here is how var, let, and const work and how important they are.

---
+++ day2-1-march/script.js
@@ -0,0 +1,92 @@
+
+console.log('Hello World');
+var a = "HELLOWWWRRRR";
+window.alert(a);
+// document.write(a);
+
+document.getElementById("topic").innerHTML = "Hii, How are ya?";
+
+b=5+5;
+console.log(b);
+
+// fullName='Aastha';
+// console.log(fullName);
+// console.log('fullName');
+
+
+var fullName = "Hande Erecel";
+console.log(fullName);
+console.log('fullName');
+
+
+
+
+// var a=10;
+// var a=25;
+// var a=100;
+// console.log(a);
+
+var x=5;
+var x=15;
+var y=25;
+var x=35;
+console.log(x);
+console.log(y);
+
+
+
+
+// fullname="Aastha Mishra";
+// FULLNAME="Mishra Aastha";
+// console.log(fullname);
+// console.log(FULLNAME);
+
+
+fullName = "Dezaz Gray"
+fullNAME = "Gray Dezaz"
+console.log(fullName);
+console.log(fullNAME); //wrong way for naming...always use Camel technique in js
+
+
+// console="a";
+// Console="a";
+// console.log(Console);  //reserved words should not be used as variables. Never Ever. 
+
+
+
+
+var company="ABC";
+comp="LMN";
+console.log(company);
+
+var company="XYZ";
+company="LION";
+console.log(company);
+
+
+// let fullName="Aastha Mishra";
+// fullName="b";
+// console.log(fullName);
+
+
+
+let lastName = "Newton";
+lastName = "f";
+console.log(lastName);
+
+
+const address="Newroad";
+// address="Kathmandu";
+// const address="Kathmandu";
+console.log(address);
+
+
+// let l;
+// l="Apple Company"; //we can give values on the other line while using the var and let but we got to define the keywords along the const while using the const(you can see in above example).
+// console.log(l);
+
+
+const w ="We are at Swift Tech Company.";
+console.log(w);
+document.write(w);
+
