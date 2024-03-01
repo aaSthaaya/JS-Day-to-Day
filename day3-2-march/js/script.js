dd81aa4fb71d0d6d3e9e3b69359be74e0e585be9
Aastha <aasthamishra175@gmail.com>
2024-03-01 13:46:33 +0545

    Worked on some javascript outputs and javascript statements

---
+++ day3-2-march/js/script.js
@@ -0,0 +1,35 @@
+
+
+document.getElementById('play').innerHTML = 7 - 5;
+
+console.log('Aastha');
+
+console.log(5 + 6);
+
+// alert('Hey There !');
+
+// onclick="document.write(2+3)";
+
+let x, y, z;
+x = 2;
+y = 7;
+z = x - y;
+
+
+document.getElementById('play').innerHTML =
+    "The value of z is" + z + ".";
+   
+document.getElementById('demo').innerHTML =
+    "Hi, I am Siri.";
+
+let person = 'Sunny';
+
+document.getElementById('demo').innerHTML='First person is ' + person + "." ;
+
+console.log('Sunny');
+
+
+
+
+
+
