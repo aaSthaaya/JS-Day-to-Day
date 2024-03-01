fae4aa3d1965dce76008d0628e544d76cb3d420f
Aastha <aasthamishra175@gmail.com>
2024-03-01 14:40:15 +0545

    tried the function in javascript and JS statements

--- day3-2-march/js/script.js
+++ day3-2-march/js/script.js
@@ -18,16 +18,21 @@ z = x - y;
 
 document.getElementById('play').innerHTML =
     "The value of z is" + z + ".";
-   
+
 document.getElementById('demo').innerHTML =
     "Hi, I am Siri.";
 
 let person = 'Sunny';
 
-document.getElementById('demo').innerHTML='First person is ' + person + "." ;
+document.getElementById('demo').innerHTML = 'First person is ' + person + ".";
 
 console.log('Sunny');
 
+
+function myFunction() {
+    document.getElementById('demo2').innerHTML = 'My Name is Valentine.';
+    document.getElementById('demo3').innerHTML = 'Hometown California.';
+}
 
 
 
