d2c999ac33e9f26eb529d18b279bb5542887e5f3
Aastha <aasthamishra175@gmail.com>
2024-03-04 12:49:06 +0545

    Worked with javascript syntax, comments, variables and their features, also the JS Let

--- day4-4-march/script.js
+++ day4-4-march/script.js
@@ -26,3 +26,25 @@ const price_acerlaptop = 87000;
 const price_delllaptop = 92000;
 let total_price = price_acerlaptop + price_delllaptop;
 document.getElementById("demo5").innerHTML = "The total price of the laptop is: " + total_price;
+
+
+const pi = 3.1415;
+let person = "John Doe";
+let message = "Hi, Yup I am unknown for your information.";
+document.getElementById("demo6").innerHTML = pi + "<br>" + person + "<br>" + message;
+
+var p = 3777;
+{
+    var p = 873645;
+}
+document.getElementById("demo7").innerHTML = p;
+// We can redeclare using var as it is global scope
+let r = 5634;
+{
+    let r = 84530;
+}
+document.getElementById("demo7").innerHTML = r;
+// We cannot redeclare using let or const keywords
+
+
+
