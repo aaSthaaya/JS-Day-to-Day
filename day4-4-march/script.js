3d3302130659f0a5b8934091b02eb2a7cdf9b05d
Aastha <aasthamishra175@gmail.com>
2024-03-04 11:04:41 +0545

    Working with keywords - var, let, const

---
+++ day4-4-march/script.js
@@ -0,0 +1,23 @@
+
+
+let firstName, lastname;
+firstName = "Rosy";
+lastname = "Camel";
+document.getElementById("demo1").innerHTML = firstName;
+document.getElementById("demo2").innerHTML = lastname;
+
+let x, y;
+x = 7;
+y = x - 7;
+document.getElementById("demo3").innerHTML = y;
+
+
+document.getElementById("myH").innerHTML = "MAIN HEADING";
+document.getElementById("myP").innerHTML = "paragraph";
+
+const k = 4;
+const l = 5;
+const m = k * l;
+document.getElementById("demo4").innerHTML = "The value of m is " + m;
+
+
