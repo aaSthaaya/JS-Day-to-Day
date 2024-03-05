78369e8e160b9206340c3242a29ac60c70d85c42
Aastha <aasthamishra175@gmail.com>
2024-03-05 11:56:08 +0545

    Today's topic assignment operator, comparision operator,if conditions, switch statement and others

---
+++ day5-5-march/script.js
@@ -0,0 +1,40 @@
+
+const num1 = 123;
+const num2 = 456;
+const sub = 1000 - num1;
+document.write(sub);
+console.log(sub);
+
+const firstName = "Aastha";
+const lastName = "Mishra";
+document.write("<br>");
+document.write(firstName + " " + lastName);
+console.log(firstName + " " + lastName);
+
+document.write("<br>");
+let num4 = 45;
+num4 *= 40;
+document.write(num4);
+
+document.write("<br>");
+let marks = 79.88 + "%";
+
+if (marks >= 90 + "%") {
+    document.write("Congratulations, You are extraordinary.");
+} else {
+    document.write("Congratulations! You are as extraordinary as others, marks doesnot define your future.");
+}
+
+document.write("<br>");
+let x = "68";
+let y = 68;
+if (x === y) {
+    document.write("These are similar datas.");
+}
+else {
+    document.write("These are different datas.");
+}
+
+
+
+
