3da9bc7991336952406559161b095197a1fde4ec
Aastha <aasthamishra175@gmail.com>
2024-03-06 17:35:31 +0545

    Using function to convert fahrenheit to celsius.

--- day6-6march/script.js
+++ day6-6march/script.js
@@ -23,17 +23,29 @@ const cars = ["Royace", " Volvo", " BMW"];
 document.write(cars);
 
 
-
+document.write("<br>");
 function myFunction(a1, a2) {
     return a1 * a2;
 }
 
-document.write("<br>");
+
 let result = myFunction(4, 3);
 document.write(result);
 
+// let y = myFunction(33, 44);
+// document.getElementById("demo").innerHTML = y;
 
+// function myFunction(s, t) {
+//     return s % t;
+// }
 
+document.write("<br>");
+
+function toCelsius(f) {
+    return (5 / 9) * (f - 32);
+}
+let value = toCelsius(97);
+document.getElementById("demo2").innerHTML = value;
 
 
 
