18f5cd8e100363dcead30196aa0dc9333f3f45fc
Aastha <aasthamishra175@gmail.com>
2024-03-06 12:05:08 +0545

    working with the datatypes and also trying to get familiar with the function, object, event, strings.

---
+++ day6-6march/script.js
@@ -0,0 +1,20 @@
+
+let a = 890;
+a++;
+let numb = a;
+document.write("The incremented number is " + numb);
+
+document.write("<br>");
+var x = 40;
+var y = 567;
+var z = 734;
+const r = x + y / z;
+document.write(r);
+
+console.log(r);
+
+document.write("<br>");
+let s = "Benz" + 75 + 76;
+document.write(s);
+
+
