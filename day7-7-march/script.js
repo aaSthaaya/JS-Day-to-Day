4de0c3fd0cb4950ece03a079d00a578c1515f0e1
Aastha <aasthamishra175@gmail.com>
2024-03-07 11:09:18 +0545

    while loop in javascript

---
+++ day7-7-march/script.js
@@ -0,0 +1,27 @@
+
+var x = 2;
+while (x <= 50) {
+    document.write(x);
+    x++;
+    document.write("<br>");
+
+}
+document.write("<br>");
+let y = 5;
+while (y <= 20) {
+    document.write(y);
+    y++;
+    document.write("<br>");
+
+}
+document.write("<br>");
+let i = 16;
+let j = 1;
+while (i <= 160) {
+    document.write("16" + "*" + j + "=" + i);
+    i = i + 16;
+    j++;
+    document.write("<br>");
+   
+}
+
