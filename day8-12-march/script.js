9c0e63713373f7a9078f636f951bd2af6d3bc4f9
Aastha <aasthamishra175@gmail.com>
2024-03-12 15:59:07 +0545

    some features of keywords, also importance of camelCase in js, also tried the string indexing.

--- day8-12-march/script.js
+++ day8-12-march/script.js
@@ -1,9 +1,45 @@
 
 console.log("Hello There!!!!");
 
+
 const x = myFunction(77, 90);
 document.getElementById("demo").innerHTML = x;
 
 function myFunction(p, q) {
     return p * q;
 }
+
+
+var firstName = "Aastha";
+console.log(firstName);
+
+firstName = "Mishra";
+console.log(firstName);
+
+const value1 = 10;
+console.log(value1 ** 3);
+
+// In javascript $ sign and _ 'underscore' are considered as letters but other signs are not considered as a letter.
+
+const $_lastname = "Newton";
+console.log($_lastname);
+
+let c;
+c = 66 / 6;
+console.log(c);
+
+const pi=3.1415;
+console.log(pi**4);
+
+// let str = "How you doin'";
+// let result = str.indexOf("you");
+
+// document.getElementById("demo1").innerHTML = result;
+
+let string="hgfuyrqiefgbvbdauilfgbjehrvbfpeuvb";
+
+console.log(string.length);
+console.log(string[string.length-4]);
+
+
+
