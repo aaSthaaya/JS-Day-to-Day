8d795337cc0283629541972fad44ecf4b0c8ab57
Aastha <aasthamishra175@gmail.com>
2024-03-13 12:08:02 +0545

    Extracting String Parts

--- day9-13-march/script.js
+++ day9-13-march/script.js
@@ -30,3 +30,23 @@ console.log(grade.charCodeAt(1)); //how charCodeAt() works
 
 console.log(grade[1]); //or we can directly use bracked notation [] that returns an indexed element from a string.
 
+const fruits = "Banana, Grapes, Litchi, Apple, Kiwi, Avocardo";
+// console.log(fruits);
+
+// console.log(fruits.trim());
+
+console.log(fruits.length);
+
+console.log(fruits.slice(8, 22)); //slice(start,end)
+
+console.log(fruits.substring(31, 45)); //substring(start,end)
+
+console.log(fruits.substr(23, 6)); // substr(start,length)
+
+console.log(fruits.substr(-8)); //if the first parameter is negative, the position counts from the end of the string.
+
+console.log(fruits.substring(0, 6));
+
+
+
+
