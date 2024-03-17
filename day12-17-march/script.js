9b44c4fceda99eb92a33c3d483122c306597876f
Aastha <aasthamishra175@gmail.com>
2024-03-17 12:14:26 +0545

    Array methods[push & pop, unshift & shift]

--- day12-17-march/script.js
+++ day12-17-march/script.js
@@ -3,6 +3,73 @@ console.log("Hie again, we can do this!");
 
 //Array
 
-let fruits = ["Mangoes", "Apples", "Banana", "Litchi", "Pomegranate"];
-console.log(fruits[4]);
+const fruits = ["Mangoes", "Apples", "Banana", "Litchi", "Pomegranate"];
+console.log(fruits[0]);
+
+let tShirts = ["Crop", "Over-Sized", "Plain", "Florist", "Silk"];
+console.log(tShirts);
+tShirts[3] = "Black Over-Sized";
+console.log(typeof tShirts, tShirts);
+console.log(Array.isArray(tShirts));
+
+let num = 24;
+console.log(Array.isArray(num));
+
+
+// Array Methods
+//Push Pop Method
+
+//push
+
+const eyeColor = ["Brown", "Blue", "Red", "Black", "Green"];
+console.log(eyeColor);
+eyeColor.push("Grey");
+console.log(eyeColor);
+
+//pop
+
+const hairColor = ["Brown", "Blue", "Red", "Black", "Green"];
+console.log(hairColor);
+let poppedHairColor = hairColor.pop();
+console.log(hairColor);
+console.log(`The popped haircolor is ${poppedHairColor}.`);
+
+//Array.push()_Example
+
+const animals = ["Cow", "Buffalo", "Sheep", "Goat", "Hen", "Dog"];
+// const count = animals.push("Pigs", "Gorilla", "Chiuwawa");
+// console.log(count);
+console.log(animals);
+
+animals.push('Ape', 'Fox', 'Bear');
+console.log(animals);
+
+const sports = ['Football', 'Badminton', 'Rugby', 'Cricket'];
+const total = sports.push('Swimming', 'Relay');
+console.log(total);
+console.log(sports);
+
+//Here is a simple example demonstrating the use of `count` as a counter variable.
+
+let count = 0; //initialize the counter
+const myArray = [1, 2, 3, 4, 5];
+for (let i = 0; i < myArray.length; i++) {
+    count++; //increment the counter
+    console.log(`The processing element ${myArray[i]} with count ${count}.`);
+}
+console.log(`The final count is ${count}.`);
+
+//Unshift Method
+
+// const vegetables = ['Potato', 'Tomato', 'Zuccinni', 'Mushroom', 'Cauliflower'];
+// overallVegetables = vegetables.unshift('Chilly', 'Spinach');
+// console.log(`The total amount of vegetables are ${overallVegetables}.`);
+// console.log(vegetables);
+
+//Shift Method
+
+const vegetables = ['Potato', 'Tomato', 'Zuccinni', 'Mushroom', 'Cauliflower'];
+overallVegetables = vegetables.shift();
+console.log(vegetables);
+
 
