ba42f249f802e37e21cdb02a4c48164de2024dff
Aastha <aasthamishra175@gmail.com>
2024-03-17 14:55:19 +0545

    Primitive vs Reference Data Type

--- day12-17-march/script.js
+++ day12-17-march/script.js
@@ -73,3 +73,21 @@ overallVegetables = vegetables.shift();
 console.log(vegetables);
 
 
+//Primitive Data Type
+
+let num1 = 20;
+let num2 = num1;
+ num1 = 40;
+ console.log(num1);
+ console.log(num2);
+
+//Reference Data Type
+
+let array1 = ['item1', 'item2'];
+let array2 = array1;
+console.log('array1', array1);
+console.log('array2', array2);
+array1.push('item3');
+console.log('After pushing element to array1, we get:');
+console.log('array1', array1);
+console.log('array2', array2);
