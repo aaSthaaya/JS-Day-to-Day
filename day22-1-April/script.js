8d323fba451646d485a698337b3eddd3f2a84c25
Aastha <aasthamishra175@gmail.com>
2024-04-01 17:29:17 +0545

    Prototypes in JS that have states and behaviours

---
+++ day22-1-April/script.js
@@ -0,0 +1,9 @@
+console.log("Hie Again!");
+
+const student = {
+    fullName: "Aastha Mishra",
+    marks: 80,
+    printMarks: function () {
+        console.log("marks = ", this.marks);
+    },
+};
