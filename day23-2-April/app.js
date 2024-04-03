afb1ef22d2c6dd3c6d5081e34b7bc7d7d4410487
Aastha <aasthamishra175@gmail.com>
2024-04-03 12:55:11 +0545

    JavaScript Prototypes, Classes, Objects, Constructor, Inheritance, Super Keyword and Error Handling.

--- day23-2-April/app.js
+++ day23-2-April/app.js
@@ -1,7 +1,8 @@
-//Prototype in JS 
-//All JavaScript objects inherit properties and methods from a prototype.
+
 console.log("Hie, Here are we...");
 
+//Prototype in JS 
+//All JavaScript objects inherit properties and methods from a prototype.
 
 const employee = {
     calcTax() {
@@ -37,12 +38,12 @@ class ToyotaCar {
 
 let fortunerCar = new ToyotaCar();
 fortunerCar.setBrand("Fortuner");
-console.log(fortunerCar.brandName); 
+console.log(fortunerCar.brandName);
 // Output: Fortuner
 
 let lexus = new ToyotaCar();
 lexus.setBrand("Lexus");
-console.log(lexus.brandName); 
+console.log(lexus.brandName);
 // Output: Lexus
 
 
@@ -53,10 +54,107 @@ console.log(lexus.brandName);
 
 class Polygon {
     constructor() {
-      this.name = 'Polygon is my name.';
+        this.name = 'Polygon is my name.';
     }
-  }
-  
-  const poly1 = new Polygon();
-  
-  console.log(poly1.name);
+}
+
+const poly1 = new Polygon();
+
+console.log(poly1.name);
+
+
+//
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+}
+
+//Inheritance -  Parent-Child relationship
+
+class person {
+    constructor() {
+        this.species = "We are homo sapien sapien.";
+    }
+    eat() {
+        console.log("Eats Food.");
+    }
+
+    sleep() {
+        console.log("Sleep Nicely.");
+    }
+}
+class Engineer extends person {
+    work() {
+        console.log("Work Wisely.");
+    }
+}
+class Painter extends person {
+    canva() {
+        console.log("Work With Colors");
+    }
+}
+let myObj = new Engineer();
+
+
+
+//Super Keyword
+
+
+
+//Create a class User with 2 properties  name & email. It has a method called viewData() that allows the user to view website data.
+
+let DATA = "Victoria's Secret."
+
+class User {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+    viewData() {
+        console.log("Data : ", DATA);
+    }
+}
+
+let student1 = new User("Albert", "albert@mail.com");
+let student2 = new User("Aristotle", "aristotle@mail.com");
+console.log(student1, student2);
+console.log(student1.viewData());
+console.log(student2.viewData());
+
+
+//Create a new class called Admin which inherits from User. Add a new method called editData to Admin that allows to edit website data.
+
+class Admin extends User {
+    constructor(name, email) {
+        super(name, email);    //User class constructor got invoked by the super key in the derived class.
+    }
+    editData() {
+        DATA = "We have new value in the existing data.";
+    }
+}
+let admin1 = new Admin("Admin", "admin@mail.com");
+console.log(admin1);
+
+
+
+
+//Error Handling in JavaScript.
+
+let a = 33.3;
+let b = 55.9;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+try {
+    console.log(a - c);
+} catch (err) {
+    console.log(err);
+}
+
+console.log(a + b);
